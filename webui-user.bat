9b0499c4847317e9eec3b2c4936c281c834a8aaa
rubenlopez16 <ruben.7.lopez@gmail.com>
2023-03-16 19:17:04 +0100

    add command line args

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--share --disable-safe-unpickle --no-half-vae --xformers --reinstall-xformers --enable-insecure-extension-access
 
 call webui.bat
